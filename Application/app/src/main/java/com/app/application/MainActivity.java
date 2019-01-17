e7fa4a76d90e2ab35f4bf820b73494726ae6b3b8
SeulYoung <38755691+SeulYoung@users.noreply.github.com>
2019-01-17 21:34:31 +0800

    app请求数据（测试用）

--- Application/app/src/main/java/com/app/application/MainActivity.java
+++ Application/app/src/main/java/com/app/application/MainActivity.java
@@ -1,11 +1,21 @@
 package com.app.application;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    requestUsingHttpURLConnection();
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
@@ -36,9 +46,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                mTextMessage.setText(msg.obj.toString());
+            }
+        }
+    };
+
+    private void requestUsingHttpURLConnection() {
+        // 网络通信属于典型的耗时操作，开启新线程进行网络请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection;
+                try {
+                    URL url = new URL("http://10.0.2.2:8080/users/aaa"); // 声明一个URL,注意——如果用百度首页实验，请使用https
+                    connection = (HttpURLConnection) url.openConnection(); // 打开该URL连接
+                    connection.setRequestMethod("GET"); // 设置请求方法，“POST或GET”，我们这里用GET，在说到POST的时候再用POST
+                    connection.setConnectTimeout(8000); // 设置连接建立的超时时间
+                    connection.setReadTimeout(8000); // 设置网络报文收发超时时间
+                    InputStream in = connection.getInputStream();  // 通过连接的输入流获取下发报文，然后就是Java的流处理
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+
+                    Message msg = new Message();
+                    msg.what = 1;
+                    msg.obj = response.toString();
+                    handler.sendMessage(msg);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 }
